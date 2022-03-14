2aa1204bd942d6534ab0db83e672cb822d7557cf
DisurIE <96732372+DisurIE@users.noreply.github.com>
2022-03-14 19:07:47 +0400

    first task full

---
+++ PracticaFirstTask/PracticaFirstTask/Window3.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PracticaFirstTask
+{
+    /// <summary>
+    /// Логика взаимодействия для Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow sas = new MainWindow();
+            sas.Show();
+            this.Close();
+
+        }
+    }
+}
